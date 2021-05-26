df103a0c8741f339495f168c61a039159c336d5e
kelly hondula <hondula@battelleecology.org>
2021-05-26 12:19:35 -0400

    updating metadata files

--- metadata/generate_metadata.R
+++ metadata/generate_metadata.R
@@ -4,11 +4,17 @@ library(neon4cast)
 test_forecast <- readr::read_csv("https://data.ecoforecast.org/forecasts/aquatics/aquatics-2021-03-01-EFInull.csv.gz")
 test_forecast %>% write_csv('metadata/aquatics-test-forecast.csv')
 
-my_forecast_file <- 'metadata/aquatics-test-forecast.csv'
+# only run if YML needs to be recreated entirely
+# create_model_metadata(forecast_file = my_forecast_file)
 
-create_model_metadata(forecast_file = my_forecast_file)
-
-write_metadata_eml(forecast_file = 'aquatics-2021-05-21-name10char.csv',
-                   metadata_yaml = 'aquatics-2021-05-21-name10char.yml',
+# To create XML (despite name of fxn)
+# for BARC lake model
+write_metadata_eml(forecast_file = 'metadata/aquatics-test-forecast.csv',
+                   metadata_yaml = 'metadata/aquatics-2021-05-01-beyondBARC.yml',
+                   forecast_issue_time = Sys.Date(),
+                   forecast_iteration_id = '1')
+# for POSE stream model
+write_metadata_eml(forecast_file = 'metadata/aquatics-test-forecast.csv',
+                   metadata_yaml = 'metadata/aquatics-2021-05-01-beyondPOSE.yml',
                    forecast_issue_time = Sys.Date(),
                    forecast_iteration_id = '1')
