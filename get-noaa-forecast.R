2db9da04eb6eedbf168a308b3dd2c526c21f8c02
kelly hondula <hondula@battelleecology.org>
2021-05-26 11:51:43 -0400

    function to aggregate forecast to daily

--- get-noaa-forecast.R
+++ get-noaa-forecast.R
@@ -3,10 +3,24 @@
 library(neon4cast)
 library(tidyverse)
 
-download_noaa('POSE', date = '2021-05-01', dir = 'noaa_POSE')
-noaa_pose <- stack_noaa('noaa_POSE')
-noaa_pose %>% write_csv('noaa-POSE-2021-05-01.csv')
+save_daily_forecast <- function(siteID, date, my_dir, my_filename){
+  download_noaa(siteID, date, my_dir)
+  stack_df <- stack_noaa(my_dir) %>%
+    mutate(date = lubridate::as_date(time)) %>%
+    group_by(date, ensemble) %>%
+    summarize(air_temperature = mean(air_temperature),
+              air_pressure = mean(air_pressure),
+              Ed_long = mean(surface_downwelling_longwave_flux_in_air),
+              Ed_short = mean(surface_downwelling_shortwave_flux_in_air),
+              precip = mean(precipitation_flux),
+              specific_humidity = mean(specific_humidity),
+              cloud_area = mean(cloud_area_fraction),
+              wind_speed = mean(wind_speed))
+  stack_df %>% write_csv(filename)
+  message(glue('saved forecast for {siteID} starting {date} as {my_filename}'))
+  return(stack_df)
+}
 
-download_noaa('BARC', date = '2021-05-01', dir = 'noaa_BARC')
-noaa_barc <- stack_noaa('noaa_BARC')
-noaa_barc %>% write_csv('noaa-BARC-2021-05-01.csv')
+save_daily_forecast('POSE', date = '2021-05-01', my_dir = 'noaa_POSE', my_filename = 'noaa-POSE-2021-05-01.csv')
+save_daily_forecast('BARC', date = '2021-05-01', my_dir = 'noaa_POSE', my_filename = 'noaa-BARC-2021-05-01.csv')
+
