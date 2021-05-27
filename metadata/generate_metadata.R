daadc847f00451c5c23256c74d7aea380eb3be4f
kelly hondula <hondula@battelleecology.org>
2021-05-27 15:41:19 -0400

    updating yml for separate teams

--- metadata/generate_metadata.R
+++ metadata/generate_metadata.R
@@ -8,13 +8,17 @@ test_forecast %>% write_csv('metadata/aquatics-test-forecast.csv')
 # create_model_metadata(forecast_file = my_forecast_file)
 
 # To create XML (despite name of fxn)
-# for BARC lake model
-write_metadata_eml(forecast_file = 'metadata/aquatics-test-forecast.csv',
-                   metadata_yaml = 'metadata/aquatics-2021-05-01-beyondBARC.yml',
+# for BARC lake model (BBTW)
+write_metadata_eml(forecast_file = 'metadata/aquatics-2021-05-01-BBTW.csv',
+                   metadata_yaml = 'metadata/aquatics-2021-05-01-BBTW.yml',
                    forecast_issue_time = Sys.Date(),
                    forecast_iteration_id = '1')
-# for POSE stream model
-write_metadata_eml(forecast_file = 'metadata/aquatics-test-forecast.csv',
-                   metadata_yaml = 'metadata/aquatics-2021-05-01-beyondPOSE.yml',
+# for POSE stream model (BTW)
+write_metadata_eml(forecast_file = 'metadata/aquatics-2021-05-01-BTW.csv',
+                   metadata_yaml = 'metadata/aquatics-2021-05-01-BTW.yml',
                    forecast_issue_time = Sys.Date(),
                    forecast_iteration_id = '1')
+
+# Where year, month, and day are the year, month, and day for the first day of
+# the submitted forecast and the team_name_ID is the code for the team name
+# that is specified in the registration (team_name_ID is a 10 character name with no spaces in it).
